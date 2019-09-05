776025be415980e67eec6445d24acb7c506a7469
Teboho <Teboho@DESKTOP-89S78R2>
2019-09-05 10:22:35 +0200

    Added a checkbox and code for close and checkbox controls

--- GitTrainingApp/GitTrainingApp/Form1.Designer.cs
+++ GitTrainingApp/GitTrainingApp/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
 			this.btnClose = new System.Windows.Forms.Button();
+			this.chbxClickMe = new System.Windows.Forms.CheckBox();
 			this.SuspendLayout();
 			// 
 			// btnClose
@@ -39,22 +40,37 @@
 			this.btnClose.TabIndex = 0;
 			this.btnClose.Text = "Close";
 			this.btnClose.UseVisualStyleBackColor = true;
+			this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+			// 
+			// chbxClickMe
+			// 
+			this.chbxClickMe.AutoSize = true;
+			this.chbxClickMe.Location = new System.Drawing.Point(144, 147);
+			this.chbxClickMe.Name = "chbxClickMe";
+			this.chbxClickMe.Size = new System.Drawing.Size(75, 17);
+			this.chbxClickMe.TabIndex = 1;
+			this.chbxClickMe.Text = "Check Me";
+			this.chbxClickMe.UseVisualStyleBackColor = true;
+			this.chbxClickMe.CheckedChanged += new System.EventHandler(this.chbxClickMe_CheckedChanged);
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.chbxClickMe);
 			this.Controls.Add(this.btnClose);
 			this.Name = "Form1";
 			this.Text = "Form1";
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
 		#endregion
 
 		private System.Windows.Forms.Button btnClose;
+		private System.Windows.Forms.CheckBox chbxClickMe;
 	}
 }
 
--- GitTrainingApp/GitTrainingApp/Form1.cs
+++ GitTrainingApp/GitTrainingApp/Form1.cs
@@ -16,5 +16,15 @@ namespace GitTrainingApp
         {
             InitializeComponent();
         }
-    }
+
+		private void btnClose_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+
+		private void chbxClickMe_CheckedChanged(object sender, EventArgs e)
+		{
+			MessageBox.Show("Thank you for checking me up!!!");
+		}
+	}
 }
