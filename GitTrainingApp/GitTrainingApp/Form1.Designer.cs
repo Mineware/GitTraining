23125e5f0318b8f404ef38900fa4be56bb57d495
CallieSeyfferdt <50356394+CallieSeyfferdt@users.noreply.github.com>
2019-09-11 15:10:06 +0200

    Neil Test Commit

--- GitTrainingApp/GitTrainingApp/Form1.Designer.cs
+++ GitTrainingApp/GitTrainingApp/Form1.Designer.cs
@@ -31,13 +31,15 @@
             this.btnClose = new System.Windows.Forms.Button();
             this.chbxClickMe = new System.Windows.Forms.CheckBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // btnClose
             // 
-            this.btnClose.Location = new System.Drawing.Point(169, 43);
+            this.btnClose.Location = new System.Drawing.Point(225, 53);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.Size = new System.Drawing.Size(100, 28);
             this.btnClose.TabIndex = 0;
             this.btnClose.Text = "Close";
             this.btnClose.UseVisualStyleBackColor = true;
@@ -46,9 +48,10 @@
             // chbxClickMe
             // 
             this.chbxClickMe.AutoSize = true;
-            this.chbxClickMe.Location = new System.Drawing.Point(144, 147);
+            this.chbxClickMe.Location = new System.Drawing.Point(192, 181);
+            this.chbxClickMe.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.chbxClickMe.Name = "chbxClickMe";
-            this.chbxClickMe.Size = new System.Drawing.Size(75, 17);
+            this.chbxClickMe.Size = new System.Drawing.Size(92, 21);
             this.chbxClickMe.TabIndex = 1;
             this.chbxClickMe.Text = "Check Me";
             this.chbxClickMe.UseVisualStyleBackColor = true;
@@ -57,20 +60,32 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(406, 152);
+            this.label1.Location = new System.Drawing.Point(541, 187);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(46, 17);
             this.label1.TabIndex = 2;
             this.label1.Text = "label1";
             // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(734, 119);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(259, 372);
+            this.listBox1.TabIndex = 3;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.chbxClickMe);
             this.Controls.Add(this.btnClose);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -83,6 +98,7 @@
 		private System.Windows.Forms.Button btnClose;
 		private System.Windows.Forms.CheckBox chbxClickMe;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
