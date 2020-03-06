5056d483e4e066f14ff92c48e6c42fbb4571ac92
Dolf van den Berg <dolf@mineware.co.za>
2020-03-06 09:13:38 +0200

    Dolf added button 1

--- GitTrainingApp/GitTrainingApp/Form1.Designer.cs
+++ GitTrainingApp/GitTrainingApp/Form1.Designer.cs
@@ -28,68 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnClose = new System.Windows.Forms.Button();
-            this.chbxClickMe = new System.Windows.Forms.CheckBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.SuspendLayout();
-            // 
-            // btnClose
-            // 
-            this.btnClose.Location = new System.Drawing.Point(225, 53);
-            this.btnClose.Margin = new System.Windows.Forms.Padding(4);
-            this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(100, 28);
-            this.btnClose.TabIndex = 0;
-            this.btnClose.Text = "Close";
-            this.btnClose.UseVisualStyleBackColor = true;
-            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
-            // 
-            // chbxClickMe
-            // 
-            this.chbxClickMe.AutoSize = true;
-            this.chbxClickMe.Location = new System.Drawing.Point(192, 181);
-            this.chbxClickMe.Margin = new System.Windows.Forms.Padding(4);
-            this.chbxClickMe.Name = "chbxClickMe";
-            this.chbxClickMe.Size = new System.Drawing.Size(92, 21);
-            this.chbxClickMe.TabIndex = 1;
-            this.chbxClickMe.Text = "Check Me";
-            this.chbxClickMe.UseVisualStyleBackColor = true;
-            this.chbxClickMe.CheckedChanged += new System.EventHandler(this.chbxClickMe_CheckedChanged);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(541, 187);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(819, 119);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(174, 100);
-            this.listBox1.TabIndex = 3;
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.chbxClickMe);
-            this.Controls.Add(this.btnClose);
-            this.Margin = new System.Windows.Forms.Padding(4);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.btnClose = new System.Windows.Forms.Button();
+			this.chbxClickMe = new System.Windows.Forms.CheckBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// btnClose
+			// 
+			this.btnClose.Location = new System.Drawing.Point(169, 43);
+			this.btnClose.Name = "btnClose";
+			this.btnClose.Size = new System.Drawing.Size(75, 23);
+			this.btnClose.TabIndex = 0;
+			this.btnClose.Text = "Close";
+			this.btnClose.UseVisualStyleBackColor = true;
+			this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+			// 
+			// chbxClickMe
+			// 
+			this.chbxClickMe.AutoSize = true;
+			this.chbxClickMe.Location = new System.Drawing.Point(144, 147);
+			this.chbxClickMe.Name = "chbxClickMe";
+			this.chbxClickMe.Size = new System.Drawing.Size(75, 17);
+			this.chbxClickMe.TabIndex = 1;
+			this.chbxClickMe.Text = "Check Me";
+			this.chbxClickMe.UseVisualStyleBackColor = true;
+			this.chbxClickMe.CheckedChanged += new System.EventHandler(this.chbxClickMe_CheckedChanged);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(406, 152);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(35, 13);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "label1";
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.Location = new System.Drawing.Point(614, 97);
+			this.listBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(132, 82);
+			this.listBox1.TabIndex = 3;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(74, 343);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 4;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.chbxClickMe);
+			this.Controls.Add(this.btnClose);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
@@ -99,6 +106,7 @@
 		private System.Windows.Forms.CheckBox chbxClickMe;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
